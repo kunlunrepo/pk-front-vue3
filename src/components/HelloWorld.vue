611a9d6e6b76991f7920ab43ce6284318bc1b0be
kunlunrepo <482976531@qq.com>
2024-02-29 14:34:48 +0800

    初始化工程

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    Hello World
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="main">
+    <HelloWorld />
+  </div>
+</template>
+
+<style lang="scss">
+.main {
+  color: red;
+}
+</style>
